814be758ea491c4457d532740696f2b2accfec30
frience0 <marckfrience0@gmail.com>
2025-06-21 15:51:20 +0800

    Add registration page and update layout with navigation links

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { register } from "@/actions/auth";
+import Link from "next/link";
+import { useActionState } from "react";
+
+export default function Register() {
+  const [state, action, isPending] = useActionState(register, undefined);
+
+  return (
+    <div className="container w-1/2">
+      <h1 className="title">Register</h1>
+
+      <form action={action} className="space-y-4">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input type="password" name="confirmPassword" />
+        </div>
+
+        <div className="flex items-end gap-4">
+          <button disabled={isPending} className="btn-primary">
+            {isPending ? "Loading..." : "Register"}
+          </button>
+          <Link href="/" className="text-link">
+            or login here
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
