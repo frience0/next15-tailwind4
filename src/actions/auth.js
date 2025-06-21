9ad70e7ef45055341c453002e4d0a0193018f237
frience0 <marckfrience0@gmail.com>
2025-06-21 16:17:19 +0800

    Add registration form validation and error handling

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,13 +1,22 @@
 "use server";
 
+import { RegisterFormSchema } from "@/lib/rules";
+
 export async function register(state, formData) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const validatedFields = RegisterFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  });
 
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const confirmPassword = formData.get("confirmPassword");
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors);
 
-  console.log(email);
-  console.log(password);
-  console.log(confirmPassword);
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      email: formData.get("email"),
+    };
+  }
+
+  console.log(validatedFields);
 }
