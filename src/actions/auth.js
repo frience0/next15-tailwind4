65787979a1eb51857c54e63cf6f1e99575d665e5
frience0 <marckfrience0@gmail.com>
2025-06-24 10:47:40 +0800

    Add MongoDB integration and user registration functionality with password hashing

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,9 @@
 "use server";
 
+import { getCollection } from "@/lib/db";
 import { RegisterFormSchema } from "@/lib/rules";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
 
 export async function register(state, formData) {
   const validatedFields = RegisterFormSchema.safeParse({
@@ -18,5 +21,26 @@ export async function register(state, formData) {
     };
   }
 
-  console.log(validatedFields);
+  const { email, password } = validatedFields.data;
+
+  const userCollection = await getCollection("users");
+  if (!userCollection) return { errors: { email: "Server error!" } };
+
+  const existingUser = await userCollection.findOne({ email });
+  if (existingUser) {
+    return {
+      errors: {
+        email: "Email already exist in our database!",
+      },
+    };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const results = await userCollection.insertOne({
+    email,
+    password: hashedPassword,
+  });
+
+  redirect("/dashboard");
 }
